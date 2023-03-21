a7f1c971ad617a2bc455e5ffd707ea9e82e4140f
JuTeu <juho.teulahti@gmail.com>
2023-03-21 11:27:50 +0200

    Edistystä karttageneraatiossa, tein huonetunnistin spritejä

--- Assets/Code/MapGeneration/TileTest.cs
+++ Assets/Code/MapGeneration/TileTest.cs
@@ -3,33 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEngine.SceneManagement;
-//using MapGenerator;
 
 public class TileTest : MonoBehaviour
 {
-    [SerializeField] private Tilemap rooms;
+    const int ice = 0;
+    const int kelp = 1;
+    const int seabed = 2;
+    const int shallow = 3;
+    const int deep = 4;
+    const int abyss = 5;
     [SerializeField] private Tilemap map;
     [SerializeField] private Vector2 startRoomPosition;
-    Tilemap roomssssssssss;
+    Tilemap rooms;
+
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(BuildMap("SampleRooms"));
+        StartCoroutine(BuildMap(-1));
     }
-    IEnumerator BuildMap(string rooms)
+    IEnumerator BuildMap(int mapNum)
     {
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(rooms, LoadSceneMode.Additive);
-        while (!asyncLoad.isDone)
-        {
-            yield return null;
-        }
-        GameObject toimi = GameObject.Find("Rooms");
-        roomssssssssss = toimi.GetComponent<Tilemap>();
+        string roomsScene = "SampleRooms";
         int mapWidth = 9;
         int mapHeight = 5;
         int roomCount = 16;
         int minBottomWidth = 1;
         int maxBottomWidth = 2;
+        if(mapNum == ice)
+        {
+            roomsScene = "IceRooms";
+            mapWidth = 9;
+            mapHeight = 5;
+            roomCount = 16;
+            minBottomWidth = 1;
+            maxBottomWidth = 2;
+        }
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(roomsScene, LoadSceneMode.Additive);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+        GameObject toimi = GameObject.Find("Rooms");
+        rooms = toimi.GetComponent<Tilemap>();
         MapGenerator mapGenerator = new MapGenerator();
         mapGenerator.GenerateMap(mapWidth, mapHeight, roomCount, minBottomWidth, maxBottomWidth);
         int[,] generatedMap = mapGenerator.GetMap();
@@ -41,20 +56,14 @@ public class TileTest : MonoBehaviour
                 PlaceRoom(j, i * -1, generatedMap[i, j]);
             }
         }
-        SceneManager.UnloadSceneAsync(rooms);
-        int huone = Random.Range(0, 15);
+        SceneManager.UnloadSceneAsync(roomsScene);
         map.RefreshAllTiles();
     }
     void PlaceRoom(int x, int y, int tile)
     {
         BoundsInt roomPosition = new BoundsInt(new Vector3Int(12 * x + ((int) startRoomPosition.x / 2), 12 * y + ((int) startRoomPosition.y), 0), size: new Vector3Int(12, 12, 1));
         BoundsInt roomSelection = new BoundsInt(new Vector3Int(0, 12 * tile + tile, 0), size: new Vector3Int(12, 12, 1));
-        TileBase[] room = roomssssssssss.GetTilesBlock(roomSelection);
+        TileBase[] room = rooms.GetTilesBlock(roomSelection);
         map.SetTilesBlock(roomPosition, room);
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
