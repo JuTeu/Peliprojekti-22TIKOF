1688d64fe9931b4a17805768264d16d5e6659782
JuTeu <juho.teulahti@gmail.com>
2023-04-19 21:09:05 +0300

    Krotilla on nyt jonkinlainen valo

---
+++ Assets/Code/Enemy/AnglerfishAI.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnglerfishAI : MonoBehaviour
+{
+    [SerializeField] Transform anglerfishLight;
+    public float speed;
+    public float checkRadius;
+    public float attackRadius;
+
+    public bool shouldRotate;
+
+    public LayerMask whatIsPlayer;
+
+    private Transform target;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    public Vector3 dir;
+
+    private bool isInChaseRange;
+    private bool isInAttackRange;
+
+    private Animator anim;
+    bool facingRight = false;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isInChaseRange = Physics2D.OverlapCircle(transform.position, checkRadius, whatIsPlayer);
+        isInAttackRange = Physics2D.OverlapCircle(transform.position, attackRadius, whatIsPlayer);
+
+        dir = target.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        dir.Normalize();
+        movement = dir;
+    }
+
+    private void FixedUpdate()
+    {
+        if (GameManager.enemiesPaused) return;
+        
+        if(isInChaseRange && isInAttackRange)
+        {
+            MoveCharacter(movement);
+        }
+        if (isInAttackRange)
+        {
+            rb.velocity = Vector2.zero;
+        }
+
+        if(dir.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if(dir.x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void MoveCharacter(Vector2 dir)
+    {
+        rb.MovePosition((Vector2)transform.position + (dir * speed * Time.deltaTime));
+    }
+
+    void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        float newLightX = anglerfishLight.localPosition.x * -1;
+        Debug.Log(newLightX);
+        anglerfishLight.localPosition = new Vector2(newLightX, anglerfishLight.localPosition.y);
+        gameObject.transform.localScale = currentScale;
+        facingRight = !facingRight;
+    }
+}
